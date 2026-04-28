2209c11e8af9a9d9cad33b5deaf1c34052c8619b
CapitalArula <smartarunak2001@gmail.com>
2026-04-28 20:18:41 +0530

    1th versiov my ArulaWeb

---
+++ Arula.js
@@ -0,0 +1,12 @@
+/*<script>
+    function  myFunction()
+        var x = document.getElementById("myTopnav");
+        if(x.className === "topnav") {
+            x.className += "reponsive"
+        } else {
+            x.className = "topnav"
+        
+        
+
+</script>*/
+  
